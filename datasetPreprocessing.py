6264d70932e2436b00f582d581ac99844464cfe5
Noel John Robert <noel060202@gmail.com>
2023-07-20 22:15:22 +0530

    datasetPreproceesing.py handles new structure

--- datasetPreprocessing.py
+++ datasetPreprocessing.py
@@ -9,42 +9,45 @@ from pathlib import Path
 
 
 def convert_xml_to_yolo(xml_file, xml_path, class_mapping, output_folder):
-    tree = etree.parse(xml_file)
-    root = tree.getroot()
+    try:
+        tree = etree.parse(xml_file)
+        root = tree.getroot()
 
-    size_element = root.find('size')
-    image_width = int(size_element.find('width').text)
-    image_height = int(size_element.find('height').text)
+        size_element = root.find('size')
+        image_width = int(size_element.find('width').text)
+        image_height = int(size_element.find('height').text)
 
-    yolo_annotations = []
-    for obj in root.findall('object'):
-        class_name = obj.find('name').text
+        yolo_annotations = []
+        for obj in root.findall('object'):
+            class_name = obj.find('name').text
 
-        if class_name not in class_mapping:
-            class_mapping[class_name] = len(class_mapping)
-        class_index = class_mapping[class_name]
+            if class_name not in class_mapping:
+                class_mapping[class_name] = len(class_mapping)
+            class_index = class_mapping[class_name]
 
-        bbox = obj.find('bndbox')
-        xmin = int(bbox.find('xmin').text)
-        ymin = int(bbox.find('ymin').text)
-        xmax = int(bbox.find('xmax').text)
-        ymax = int(bbox.find('ymax').text)
+            bbox = obj.find('bndbox')
+            xmin = int(bbox.find('xmin').text)
+            ymin = int(bbox.find('ymin').text)
+            xmax = int(bbox.find('xmax').text)
+            ymax = int(bbox.find('ymax').text)
 
-        x = (xmin + xmax) / (2.0 * image_width)
-        y = (ymin + ymax) / (2.0 * image_height)
-        w = (xmax - xmin) / image_width
-        h = (ymax - ymin) / image_height
+            x = (xmin + xmax) / (2.0 * image_width)
+            y = (ymin + ymax) / (2.0 * image_height)
+            w = (xmax - xmin) / image_width
+            h = (ymax - ymin) / image_height
 
-        yolo_annotation = f"{class_index} {x:.6f} {y:.6f} {w:.6f} {h:.6f}"
-        yolo_annotations.append(yolo_annotation)
+            yolo_annotation = f"{class_index} {x:.6f} {y:.6f} {w:.6f} {h:.6f}"
+            yolo_annotations.append(yolo_annotation)
 
-    label_filename = xml_path.replace('/', '_') + '.txt'
-    output_file = Path(output_folder, label_filename)
-    with output_file.open('w') as f:
-        f.write('\n'.join(yolo_annotations))
+        label_filename = xml_path.replace('/', '_') + '.txt'
+        output_file = Path(output_folder, label_filename)
+        with output_file.open('w') as f:
+            f.write('\n'.join(yolo_annotations))
+    except FileNotFoundError:
+        print('XML file not found: {}'.format(xml_file))
 
 
-def copy_files(source_file, dest_image_folder, dest_label_folder):
+def copy_files(source_file, dest_image_folder, dest_label_folder, idd_dataset):
     with open(source_file, 'r') as file:
         lines = file.readlines()
 
@@ -53,8 +56,8 @@ def copy_files(source_file, dest_image_folder, dest_label_folder):
 
     for line in lines:
         line = line.strip()
-        image_file = os.path.join('IDD_Detection', 'JPEGImages', line + '.jpg')
-        # label_file = os.path.join('IDD_Detection', 'Annotations', line + '.xml')
+        image_file = os.path.join(idd_dataset, 'JPEGImages', line + '.jpg')
+        # label_file = os.path.join(idd_dataset, 'Annotations', line + '.xml')
 
         image_filename = line.replace('/', '_') + '.jpg'
         # label_filename = line.replace('/', '_') + '.txt'
@@ -64,89 +67,77 @@ def copy_files(source_file, dest_image_folder, dest_label_folder):
 
         try:
             shutil.copy(image_file, dest_image_path)
-            print(f"Copied image file: {image_file} to {dest_image_path}")
+            print('Copied image file: {} to {}'.format(image_file, dest_image_path))
         except FileNotFoundError:
-            print(f"Image file not found: {image_file}")
+            print('Image file not found: {}'.format(image_file))
 
 
-def convert_dataset(xml_path_files):
+def convert_dataset(xml_path_files, idd_dataset, modified_dataset):
     class_mapping = {}
-    output_dir = Path('modified_dataset')
 
-    pbar_total = sum(len(open(file).readlines()) for file in xml_path_files)
+    pbar_total = sum(len(open(os.path.join(idd_dataset, file)).readlines()) for file in xml_path_files)
     pbar = tqdm(total = pbar_total, desc = 'Converting XML to YOLO')
 
     for xml_path_file in xml_path_files:
-        with open(xml_path_file, 'r') as file:
+        with open(os.path.join(idd_dataset, xml_path_file), 'r') as file:
             xml_paths = file.read().splitlines()
 
         # this is to create test, train and val folders inside labels folder
         new_folder = Path(xml_path_file).stem.split('.')[0]
-        output_folder = output_dir / 'labels' / new_folder
+        output_folder = modified_dataset / 'labels' / new_folder
         output_folder.mkdir(parents=True, exist_ok=True)
         
-        # test folder does not have annotations stored by default, and assuming it is chacked at last only
+        # test folder does not have annotations stored by default, and assuming it is checked at last only
         if new_folder == 'test': break  
         
         for xml_path in xml_paths:
             folder, subfolder, filename = xml_path.split("/")
-            xml_file = Path('IDD_Detection/Annotations') / folder / subfolder / (filename + '.xml')
+            xml_file = idd_dataset / 'Annotations' / folder / subfolder / (filename + '.xml')
 
             convert_xml_to_yolo(xml_file, xml_path, class_mapping, output_folder)
             pbar.update(1)
 
     pbar.close()
 
-    class_mapping_file = output_dir / 'class_mapping.json'
+    class_mapping_file = modified_dataset / 'class_mapping.json'
     with class_mapping_file.open('w') as f:
         json.dump(class_mapping, f)
         
 
 def main():
-    xml_path_files = ['IDD_Detection/train.txt', 'IDD_Detection/val.txt', 'IDD_Detection/test.txt']     # test.txt should be at last compulsorily
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    root_directory = os.path.dirname(current_directory) # /TeamTRON_MarBaseliosCollegeOfEngineeringAndTechnology_RoadObjectDetectionWithDeepLearning    
+    
+    xml_path_files = ['train.txt', 'val.txt', 'test.txt']     # test.txt should be at last compulsorily
 
     start_time = time.time()
     
     # create main directory to store modified dataset
-    output_dir = Path('modified_dataset')
-    if output_dir.exists() and output_dir.is_dir():
-        shutil.rmtree(output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
+    idd_dataset = Path(os.path.join(root_directory, 'data', 'IDD_Detection'))
+    modified_dataset = Path(os.path.join(root_directory, 'data', 'modified_dataset'))
+    if modified_dataset.exists() and modified_dataset.is_dir():
+        shutil.rmtree(modified_dataset)
+    modified_dataset.mkdir(parents=True, exist_ok=True)
 
-    convert_dataset(xml_path_files)
+    convert_dataset(xml_path_files, idd_dataset, modified_dataset)
 
-    dest_train_image_folder = 'modified_dataset/images/train'
-    dest_train_label_folder = 'modified_dataset/labels/train'
-    dest_val_image_folder = 'modified_dataset/images/val'
-    dest_val_label_folder = 'modified_dataset/labels/val'
-    dest_test_image_folder = 'modified_dataset/images/test'
-    dest_test_label_folder = 'modified_dataset/labels/test'
+    # initialize path for output folders
+    dest_train_image_folder = os.path.join(modified_dataset, 'images', 'train')
+    dest_train_label_folder = os.path.join(modified_dataset, 'labels', 'train')
+    dest_val_image_folder = os.path.join(modified_dataset, 'images', 'val')
+    dest_val_label_folder = os.path.join(modified_dataset, 'labels', 'val')
+    dest_test_image_folder = os.path.join(modified_dataset, 'images', 'test')
+    dest_test_label_folder = os.path.join(modified_dataset, 'labels', 'test')
 
-    copy_files(xml_path_files[0], dest_train_image_folder, dest_train_label_folder)
-    copy_files(xml_path_files[1], dest_val_image_folder, dest_val_label_folder)
-    copy_files(xml_path_files[2], dest_test_image_folder, dest_test_label_folder)
+    # copy files to modified_dataset
+    copy_files(os.path.join(idd_dataset, xml_path_files[0]), dest_train_image_folder, dest_train_label_folder, idd_dataset)
+    copy_files(os.path.join(idd_dataset, xml_path_files[1]), dest_val_image_folder, dest_val_label_folder, idd_dataset)
+    copy_files(os.path.join(idd_dataset, xml_path_files[2]), dest_test_image_folder, dest_test_label_folder, idd_dataset)
     
-    # pbar_copy = tqdm(total = 3, desc = 'Copying Files')
-    
-    # pbar_copy.set_description('Copying train files')
-    # copy_files(xml_path_files[0], dest_train_image_folder, dest_train_label_folder)
-    # pbar_copy.update(1)
-    
-    # pbar_copy.set_description('Copying val files')
-    # copy_files(xml_path_files[1], dest_val_image_folder, dest_val_label_folder)
-    # pbar_copy.update(1)
-    
-    # pbar_copy.set_description('Copying test files')
-    # copy_files(xml_path_files[2], dest_test_image_folder, dest_test_label_folder)
-    # pbar_copy.update(1)
-    
-    # pbar_copy.close()
-
-
     end_time = time.time()
     execution_time = end_time - start_time
 
-    print("Execution completed in {:.5f} seconds.".format(execution_time))
+    print('Execution completed in {:.5f} seconds'.format(execution_time))
 
 
 if __name__ == '__main__':
